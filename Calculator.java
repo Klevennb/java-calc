9ab83caca36be6667314a4950c6b5b4458e2378c
Matt Kleven <klevennb115@gmail.com>
2019-04-16 23:56:07 -0500

    calculator compiled and working

--- Calculator.java
+++ Calculator.java
@@ -6,9 +6,26 @@ public class Calculator {
         int num1=0;
         int num2=0;
         char operator;
+        double answer = 0.0;
 
         Scanner scanObject = new Scanner(System.in);
-        System.out.println("Hello World!");
+        System.out.println("Enter your first number");
+        num1 = scanObject.nextInt();
+        System.out.println("Enter your second number");
+        num2 = scanObject.nextInt();
+        System.out.println("What operator?");
+        operator = scanObject.next().charAt(0);
+        switch (operator) {
+            case '+': answer = num1 + num2;
+            break;
+            case '-': answer = num1 - num2;
+            break;
+            case '*': answer = num1 * num2;
+            break;
+            case '/': answer = num1 / num2;
+            break;
+        }
+        System.out.println(num1+ " " + operator + " "+ num2 + " = "+answer);;
 
     }
 }
