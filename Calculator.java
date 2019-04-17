285c7e12bbf02cd800130dd3d73cc942bfa5b6ed
Matt Kleven <klevennb115@gmail.com>
2019-04-16 23:43:19 -0500

    Skeleton of Calculator

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int num1=0;
+        int num2=0;
+        char operator;
+
+        Scanner scanObject = new Scanner(System.in);
+        System.out.println("Hello World!");
+
+    }
+}
