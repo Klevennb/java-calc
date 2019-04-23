84b0ea44ace70e6aad2cb0c50328ee41ad302272
Matt Kleven <klevennb115@gmail.com>
2019-04-23 00:33:03 -0500

    refined code

--- PasswordChallenge.java
+++ PasswordChallenge.java
@@ -1,37 +1,16 @@
 public class PasswordChallenge {
-    public static StringBuilder maskify(String str) {
-        // throw new UnsupportedOperationException("Unimplemented");
+    public static String maskify(String str) {
         StringBuilder myPassword = new StringBuilder(str);
+        String newStuff = new String();
         if (str.length() > 4) {
             for (int i = 0; i < str.length()-4; i++) {
-                
-                myPassword.setCharAt(i, 'x');
-                System.out.println(myPassword);
+                myPassword.setCharAt(i, '#');
+                newStuff = myPassword.toString();
             }
-            }
-        return myPassword;
-        // }
-        // System.out.println(str);
+        }
+        return newStuff;
     }
-
     public static void main(String[] args) {
-        System.out.println("Welcome to the Weigh-In process!!");
-        // checkIfFits(100, 39);
         System.out.println(maskify("Have a nice day.."));
-
     }
-
 }
-
-// public class Throw {
-//     // static void checkIfFits(int height, int width) {
-//         if (6 > 4) {
-//             // for (int i = 0; i < str.length()-4; i++) {
-//             // // StringBuilder myPassword = new StringBuilder(str);
-//             // // myPassword.setCharAt(i, 'x');
-//             // }
-//             System.out.println("haha");
-//         }
-//     }
-
-
